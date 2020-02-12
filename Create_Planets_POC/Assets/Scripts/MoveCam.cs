ce2f4ac6ad92b8e1407bb41412fbfe32b7a9c7e9
cattia <you@example.com>
2020-02-12 13:17:56 -0500

    [cricri] projet CREATE PLANET

---
+++ Create_Planets_POC/Assets/Scripts/MoveCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+
+    private float moveSpeed = 0.5f;
+    private float scrollSpeed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+        }
+    }
+}
---
+++ Create_Planets_POC/Assets/Scripts/OnObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class OnObject : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    Vector3 pos;
+    public GameObject spawnee;
+    public Transform spawnPos;
+    Vector3 vec = new Vector3(7,0,0);
+    int nb = 0;
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                if (hit.collider.gameObject != null)
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+
+           /* if (Physics.Raycast(ray, out hit))
+            {*/
+                Instantiate(spawnee);
+                spawnee.transform.position = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+          
+
+
+        }
+
+        
+    }
+}
